3bdf6cdb6ef917622833a8e34350776154624990
JiaChengZhao1 <91324901+JiaChengZhao1@users.noreply.github.com>
2022-02-27 23:06:52 -0500

    Update MetroData.java

--- Metro/MetroData.java
+++ Metro/MetroData.java
@@ -1,9 +1,10 @@
 public class MetroData{
   public static void main(String[] args){
-    MetroCard card1 = new MetroCard("Andy Zhu", 25, true);
-    MetroCard card2 = new MetroCard("Sample", 1.50, false);
+    MetroCard card1 = new MetroCard("Andy Zhu", 25, true, 21);
+    MetroCard card2 = new MetroCard("Sample", 1.50, false, 24);
 
     card1.swipe();
+    card2.swipe();
 
     // card1.deductBalance();
     // card2.deductBalance();
