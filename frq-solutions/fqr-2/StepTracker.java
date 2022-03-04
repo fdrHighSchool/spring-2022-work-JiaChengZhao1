64fd06733b378dfde150923cf78ef23a7645d905
JiaChengZhao1 <jiachengz3@nycstudents.net>
2022-03-04 12:26:53 -0500

    update

---
+++ frq-solutions/fqr-2/StepTracker.java
@@ -0,0 +1,32 @@
+public class StepTracker{
+
+  private int steps
+  private int totalSteps
+  private int totalDays
+  private int minStep
+  private int activeDays
+
+  public StepTracker(String tS, int tD, int mS, int aD, int s){
+    this.totalSteps = tS;
+    this.totalDays = tD;
+    this.minStep = mS;
+    this.activeDays = aD;
+    this.steps = s;
+  }
+  public addDailySteps(){
+
+  }
+  public activeDays(){
+      //if steps is less than 10000, then it is not an active day
+      if (this.steps <= this.minStep){
+        return false;
+      }else{
+        return true;
+      }
+
+  }
+  public averageSteps(){
+    return totalSteps/activeDays;
+    //total number of steps from each day divided by amount of days
+  }
+}
