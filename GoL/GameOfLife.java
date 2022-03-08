3ea61238ee1d72b409e174088744780bbfb513d4
JiaChengZhao1 <jiachengz3@nycstudents.net>
2022-03-08 13:04:43 -0500

    progress

--- GoL/GameOfLife.java
+++ GoL/GameOfLife.java
@@ -1,16 +1,27 @@
-import java.utl.*;
+import java.util.*;
   public class GameOfLife{
     public static void main(String[] args) {
-      System s = new scanner(System.in);
-      String[][] world = new String[30][30];
-        setWorld();
+      String[][] world = new String[30][20];
+        setWorld(world);
+        displayWorld(world);
+        // System.out.println(life(world));
+        world[0][0] = "[ x ]";
   }
+  // public static life(String[][]world){
+  //      world[2][3] = "x";
+  // }
 
-
-  public static void setWorld(String[][]world ){
+  public static void setWorld(String[][]world){
       for(int row = 0; row < world.length; row++) {
         for(int col = 0; col < world[row].length; col++){
           world[row][col] = "[ ]";
+      }
+    }
+  }
+  public static void displayWorld(String[][]world){
+    for(int row = 0; row < world.length; row++) {
+      for(int col = 0; col < world[row].length; col++) {
+        System.out.print(world[row][col] + " ");
       }
     }
   }
