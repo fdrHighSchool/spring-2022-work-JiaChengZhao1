80bd6346c1b1a468fc0d6b4024adb095fda6666c
JiaChengZhao1 <jiachengz3@nycstudents.net>
2022-03-08 08:46:02 -0500

    startup

--- GoL/GameOfLife.java
+++ GoL/GameOfLife.java
@@ -1,8 +1,17 @@
 import java.utl.*;
-public class GameOfLife{
-  public static void main(String[] args) {
+  public class GameOfLife{
+    public static void main(String[] args) {
+      System s = new scanner(System.in);
+      String[][] world = new String[30][30];
+        setWorld();
+  }
 
+
+  public static void setWorld(String[][]world ){
+      for(int row = 0; row < world.length; row++) {
+        for(int col = 0; col < world[row].length; col++){
+          world[row][col] = "[ ]";
+      }
+    }
   }
 }
-
-public static void world()
